affaa1da495928767c2dff9429cd5122549525aa
Armelle <armelle.lecerf@orange.fr>
2021-05-29 18:33:21 +0200

    Initial Commit

---
+++ MonBalluchon/WeatherNavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherNavigationController.swift
+//  MonBalluchon
+//
+//  Created by macmini-Armelle on 26/05/2021.
+//
+
+import UIKit
+
+class WeatherNavigationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
